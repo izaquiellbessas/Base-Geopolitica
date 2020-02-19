03286b08600bec2daaf435b0534a7194c3380c2c
Izaquiel Bessas <izaquiellbessas@users.noreply.github.com>
2020-02-18 22:42:30 -0300

    Ajustando o retorno do recurso
    Ajustando o retorno do recurso Países para retornar 204 quando a consulta for realizada com sucesso, porém não há o que exibir.

--- Projeto/Geopolitica/src/main/java/com/treesoftware/geopolitica/resources/PaisesResource.java
+++ Projeto/Geopolitica/src/main/java/com/treesoftware/geopolitica/resources/PaisesResource.java
@@ -3,6 +3,7 @@ package com.treesoftware.geopolitica.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,8 +19,10 @@ public class PaisesResource {
 	private PaisesRepository repositorio;
 	
 	@GetMapping("/listar-todos")
-	public List<Paises> listarTodos() {
-		return repositorio.findAll();
+	public ResponseEntity<?> listarTodos() {
+		List<Paises> listaPaises = repositorio.findAll();
+		
+		return listaPaises.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(listaPaises);
 	}
 	
 	
